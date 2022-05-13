93f8a75988a793586edac8f6fcd4bda86b902fa9
SantamRC <santamdev404@gmail.com>
2022-05-14 02:00:18 +0530

    Changed App Functions

--- src/App.js
+++ src/App.js
@@ -1,16 +1,65 @@
-import './App.css';
-import { useEffect } from "react";
-import Router from "./Utilities/router";
-import ProtectedRoute from "./Utilities/protectedRoute";
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import { Switch, Route } from "react-router-dom";
+import { connect } from "react-redux";
+import Home from "./components/Home";
+import SearchResult from "./components/SearchResult";
+import Profile from "./components/Profile";
+import MovieInfo from "./components/MovieInfo";
+import Footer from "./components/Footer";
+import { signIn, fetchMovies } from "./actions/index";
+import { auth, createUserProfileDocument } from "./firebase/firebaseUtils";
 
-function App() {
-  useEffect(() => {});
+function App(props) {
+  const [navShow, setnavShow] = useState(true);
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        createUserProfileDocument(user);
+        props.fetchMovies(user.uid);
+      }
+      props.signIn(user);
+      props.fetchMovies(null);
+    });
+  }, [props]);
+
+  useEffect(() => {
+    const navFade = () => {
+      if (window.scrollY > 60) {
+        setnavShow(false);
+      } else setnavShow(true);
+    };
+    window.addEventListener("scroll", navFade);
+    return () => {
+      window.removeEventListener("scroll", navFade);
+    };
+  }, []);
 
   return (
     <div className="App">
-      <Router />
+      <div className="flex-wrapper">
+        <Header show={navShow} />
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/search/:query">
+            <SearchResult />
+          </Route>
+          <Route path="/movie/:id">
+            <MovieInfo />
+          </Route>
+          <Route path="/profile">
+            <Profile />
+          </Route>
+        </Switch>
+
+        <Footer />
+      </div>
     </div>
   );
 }
 
-export default App;
+export default connect(null, { signIn, fetchMovies })(App);
--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import { Provider } from "react-redux";
+import "./index.css";
+import App from "./App";
+import ScrollToTop from "./components/ScrollToTop";
+import combineReducers from "./reducers/rootReducer";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  combineReducers,
+  composeEnhancers(applyMiddleware(thunk))
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <ScrollToTop />
+        <App />
+        {/* </ScrollToTop> */}
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
