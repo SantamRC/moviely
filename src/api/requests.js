c70eda925fc99bcb4e15f8f864e1f80cf709cb80
SantamRC <santamdev404@gmail.com>
2022-05-14 16:23:29 +0530

    Added API Key

--- src/api/requests.js
+++ src/api/requests.js
@@ -1,4 +1,4 @@
-const API_KEY = process.env.REACT_APP_API_KEY;
+const API_KEY = "4d51e2149ffec1e3fabb84a54d724b76";
 
 const requests = {
   trending: `https://api.themoviedb.org/3/trending/movie/day?api_key=${API_KEY}`,
@@ -11,10 +11,15 @@ const requests = {
   romance: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&sort_by=popularity.desc&page=1&with_genres=10749`,
   thriller: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&sort_by=popularity.desc&page=1&with_genres=53`,
   images: "https://image.tmdb.org/t/p/w500/",
-  getMovie: (id) => {return `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}`},
-  getYoutube: (id) => {return `https://api.themoviedb.org/3/movie/${id}/videos?api_key=${API_KEY}`},
-  searchMovie: (query) => {return `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${query}`}
-
+  getMovie: (id) => {
+    return `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}`;
+  },
+  getYoutube: (id) => {
+    return `https://api.themoviedb.org/3/movie/${id}/videos?api_key=${API_KEY}`;
+  },
+  searchMovie: (query) => {
+    return `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${query}`;
+  },
 };
 
 export default requests;
